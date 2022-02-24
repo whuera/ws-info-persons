c7bd68bfe9959b192e151e5c98a9916ea65649cf
William Huera <whuera@gmail.com>
2022-02-23 19:50:29 -0500

    create project

---
+++ src/main/java/com/mobilpymes/services/datapersons/DataPersonsApplication.java
@@ -0,0 +1,15 @@
+package com.mobilpymes.services.datapersons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public
+class DataPersonsApplication {
+
+    public static
+    void main (String[] args) {
+        SpringApplication.run ( DataPersonsApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/mobilpymes/services/datapersons/DataPersonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilpymes.services.datapersons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPersonsApplicationTests {
+
+    @Test
+    void contextLoads ( ) {
+    }
+
+}
