c192efd8ede361fd9295d2c56c4067c7c91487a3
William Huera <whuera@gmail.com>
2022-03-02 22:39:00 -0500

    update clas

--- src/main/java/com/mobilpymes/services/datapersons/entity/Person.java
+++ src/main/java/com/mobilpymes/services/datapersons/entity/Person.java
@@ -1,5 +1,6 @@
 package com.mobilpymes.services.datapersons.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
@@ -20,4 +22,11 @@ class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @NotBlank(message = "Name is mandatory")
+    private String firstName;
+    @NotNull
+    @NotBlank(message = "LastName is mandatory")
+    private String lastName;
+    private String middleName;
 }
